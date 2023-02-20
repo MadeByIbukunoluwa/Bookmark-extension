a2a344be327cd83ae5738877644dde0fee16b463
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-02-20 06:59:58 +0100

    feat:update d background.js file with code that checks if the active tab is a youtube video

--- background.js
+++ background.js
@@ -1,12 +1,11 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
+// this block of code first checks if the active tab is a youtube page , then extracts the query paramaters
+// the part after the uestion mark to give each video a unique key value 
+
+chrome.tabs.onUpdated.addListener((tabId,tab)=>{
+  if(tab.url && tab.url.includes('youtube.com/watch')) {
+    const queryParamters = tab.url.split('?')[1];
+    const urlParameters = new URLSearchParams(queryParamters);
+
   
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+  }
+})
