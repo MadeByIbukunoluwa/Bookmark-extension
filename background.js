b331ceac39f6b085277fc0236c57fdc91e20b4ef
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-02-20 07:00:43 +0100

    feat:modified background.js to send message to contentScript that the page has loaded

--- background.js
+++ background.js
@@ -6,6 +6,10 @@ chrome.tabs.onUpdated.addListener((tabId,tab)=>{
     const queryParamters = tab.url.split('?')[1];
     const urlParameters = new URLSearchParams(queryParamters);
 
-  
+    //using thsi, theres a messaging system that happens between our extesnion , with thts we are saying that a new video has loaded , it takes a tabId, it takes in a unique object ,anf it aalso takes a callback function 
+    chrome.tabs.sendMessage(tabId,{
+      type:'NEW',
+      videoId:urlParameters.get('v')
+    })
   }
 })
